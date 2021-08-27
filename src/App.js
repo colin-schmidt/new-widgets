32d079d7d31b79b59e623013952154d4e8db2572
colinschmidt <colincarlschmidt@gmail.com>
2021-08-27 14:27:59 -0500

    add app.css padding

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
 import Header from "./components/Header";
+import "./components/App.css"
 
 const items = [
   {
